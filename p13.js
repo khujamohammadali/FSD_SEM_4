dded537783c597ec7bce6e27df78f858539cc66f
khujamohammadali <khujamohammadali79@gmail.com>
2026-03-13 13:23:55 +0530

    Add appendFile to write 'world' to a.txt

--- p13.js
+++ p13.js
@@ -3,3 +3,9 @@ fs.writeFile("a.txt","hello",(err)=>{
     if(err) throw err
     console.log("written in file successfully")
 })
+
+var fs=require("fs")
+fs.appendFile("a.txt","world",(err)=>{
+    if(err) throw err
+    console.log("written in file successfully")
+})
