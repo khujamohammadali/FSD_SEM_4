9d00f0f141e3c6eced4618457ef6f7e503e8a88c
khujamohammadali <khujamohammadali79@gmail.com>
2026-03-17 13:25:11 +0530

    Update p20.js

--- p20.js
+++ p20.js
@@ -10,3 +10,16 @@ console.log(pm.basename(a))
 console.log(pm.dirname(a))
 console.log(pm.extname(a))
 console.log(pm.parse(a))
+
+var pm=require("path")
+var a="D:/lJ/abc.html"
+var fs=require("fs")
+
+fs.mkdirSync(pm.dirname(a))
+
+if(pm.extname(a)==".txt"){
+    fs.writeFileSync(a,"This is a text file")
+}
+else {
+    fs.writeFileSync(a,'<h1 style="color:blue">This is html file</h1>')
+}
